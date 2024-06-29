54c88f7da313a35f49fa5b6fa6e8cd1c912ff4d8
Daisuke <giardinodd@yahoo.com>
2024-06-30 00:35:51 +0200

    Delete module directory

--- module/customize.sh
+++
@@ -1,8 +0,0 @@
-ui_print ""
-ui_print "################################"
-ui_print "#                              #"
-ui_print  "#      Ram Revy Gtx         #"
-ui_print "#                              #"
-ui_print "################################"
-ui_print ""
-
